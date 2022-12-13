87e06f3e719e022e117fcf38f7be2ea67bb759a4
solamsalu <solamsalu@gmail.com>
2022-12-13 12:09:00 -0800

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,17 +5,16 @@
  */
 int main(void)
 {
-  char sol[] = "_putchar";
+	char sol[] = "_putchar";
 
-  int c;
-  for (c = 0; c < 8; c++)
-    {
+	int c;
 
-      _putchar(sol[c]);
-      
-    }
-     _putchar ('\n');
-     return (0);
+	for (c = 0; c < 8; c++)
+	{
+		_putchar(sol[c]);
+
+	}
+	_putchar ('\n');
+	return (0);
+
 }
-
-
