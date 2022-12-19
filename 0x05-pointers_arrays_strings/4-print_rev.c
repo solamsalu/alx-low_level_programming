e752583dff027b79ff357bae9b1b4a1025e554c4
Solomon Amsalu <74931480+solamsalu@users.noreply.github.com>
2022-12-20 00:43:09 +0300

    Update 4-print_rev.c
    Task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,18 +6,29 @@
  *Return: string in reverse
  */
 
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+
+	return (i);
+}
+
+/**
+ * print_rev - prints a string in reverse
+ *
+ * @s: string to print in reverse
+ *
+ * Return: void
+ */
 void print_rev(char *s)
 {
 	int i;
 
-	while (s[i] != '\0')
-	{
-		i++;
-	}
-	i--;
-	while (s[i] != '\0')
-	{
-		_putchar(s[i--]);
-	}
+	for (i = _strlen(s) - 1; i >= 0; i--)
+		_putchar(s[i]);
 	_putchar('\n');
 }
