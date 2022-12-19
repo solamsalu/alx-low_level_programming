9dbb063ef96a99902e959bbb9735a35d22ab8028
solamsalu <solamsalu@gmail.com>
2022-12-19 10:50:58 -0800

    Task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,16 +1,23 @@
 #include "main.h"
 
 /**
- *_puts - a function that prints a string, followed by a new line
- *@str: input a string
- *Return: string
+ *_puts - a function that prints a string, in reverse followed by a new line
+ *@s: input a string
+ *Return: string in reverse
  */
 
-void _puts(char *str)
+void print_rev(char *s)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0'; i++)
-		_putchar(str[i]);
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	i--;
+	while (s[i] != '\0')
+	{
+		_putchar(s[i--]);
+	}
 	_putchar('\n');
 }
