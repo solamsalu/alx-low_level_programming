eb2dd9113da1ce5d8425e8e19a625becb42fab1e
Solomon Amsalu <74931480+solamsalu@users.noreply.github.com>
2022-12-22 16:34:26 +0300

    Update 1-strncat.c
    Task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,20 +9,20 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int len1 = 0;
-	int len2 = 0;
+	int len1 = 0; 	
+	int len2 = 0; 
 	int i;
 
-	while (dest[len1] != '\0')
+	while (src[len1] != '\0')
 		len1++;
 
-	while (src[len2] != '\0')
+	while (dest[len2] != '\0')
 		len2++;
 
 	for (i = 0; i < n; i++)
 	{
-		dest[len1] = src[i];
-		len1++;
+		dest[len2] = src[i];
+		len2++;
 	}
 	return (dest);
 }
