1be8ebaf4de9cf682c8bc7608303a9281c061ffa
Solomon Amsalu <74931480+solamsalu@users.noreply.github.com>
2022-12-22 20:41:00 +0300

    Update 5-string_toupper.c
    Task 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -9,13 +9,12 @@
 
 char *string_toupper(char *str)
 {
-	int i = 0;
+	int i;
 
-	while (str[i])
+	for (i = 0; str[i] != '\0'; i++)
 	{
 		if (str[i] >= 'a' && str[i] <= 'z')
 			str[i] -= 32;
-		i++;
 	}
 	
 	return (str);
