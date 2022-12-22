159ca9549d2ad78569aea554cd3063a61d269860
solamsalu <solamsalu@gmail.com>
2022-12-22 05:27:50 -0800

    Task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,10 +10,14 @@
 char *_strncat(char *dest, char *src, int n)
 {
 	int len1 = 0;
+	int len2 = 0;
 	int i;
 
 	while (dest[len1] != '\0')
 		len1++;
+
+	while (src[len2] != '\0')
+		len2++;
 
 	for (i = 0; i < n; i++)
 	{
