499913982f2f39ebedef719074c621242d36363f
Solomon Amsalu <74931480+solamsalu@users.noreply.github.com>
2022-12-22 20:31:10 +0300

    Update 5-string_toupper.c
    Task 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * string_toupper - a function that changes all lowercase letters of a string to uppercase letters.
+ * string_toupper - Changes all lowercase letters of a string to uppercase letters.
  * @str: The string to be changed.
  *
  * Return: A pointer to the changed string.
