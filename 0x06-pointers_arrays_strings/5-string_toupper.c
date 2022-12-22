4d80e3bcafd30679ac97cfef588a2ec9abf6cab4
solamsalu <solamsalu@gmail.com>
2022-12-22 09:56:37 -0800

    Task 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * string_toupper - Changes all lowercase letters of a string to uppercase letters.
+ * string_toupper - Changes lowercase letters of a string to uppercase letters.
  *
  * @str: The string to be changed
  *
