c94b068a60dad0f032f65523cef1930436048551
solamsalu <solamsalu@gmail.com>
2023-01-06 07:07:49 -0800

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main -  writes its name
+ * @argc: arguement count
+ * @argv: array of pointers to arguement strings
+ * Return: 0
+ **/
+int main(int argc, char *argv[])
+{
+	printf("%s", argv[0]);
+	return (0);
+}
