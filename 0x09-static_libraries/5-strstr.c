c1c7ccd22f3a621db83dc85b744ad83d1a1a0cd5
solamsalu <solamsalu@gmail.com>
2023-01-06 04:57:18 -0800

    Task 0

---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strstr - Entry point
+ *@haystack: input
+ *@needle: input
+ * Return: Always 0 (Success)
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 0 ; haystack[i] ; i++)
+	{
+		for (k = i, j = 0 ; needle[j] != '\0' ; j++, k++)
+		{
+			if (haystack[k] != needle[j] || haystack[k] == '\0')
+			{
+				break;
+			}
+		}
+		if (needle[j] == 0)
+		{
+			return (haystack + i);
+		}
+	}
+	return (0);
+}
