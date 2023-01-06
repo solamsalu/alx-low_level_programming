c4091910276c1247c1b0b21bfe0d65e7653e35fe
Solomon Amsalu <74931480+solamsalu@users.noreply.github.com>
2023-01-06 18:21:46 +0300

    Update 0-whatsmyname.c
    Task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
  * main -  writes its name
@@ -8,6 +9,7 @@
  **/
 int main(int argc, char *argv[])
 {
+	(void) argc;
 	printf("%s", argv[0]);
 	return (0);
 }
