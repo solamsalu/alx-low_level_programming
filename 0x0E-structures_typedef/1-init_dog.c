f5ecb828dcb330dd6c70e24f632570bccea186d6
solamsalu <solamsalu@gmail.com>
2023-01-16 01:16:24 -0800

    Task 0

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * sturct dog  - the dog structure
+ * @name: the name of the dog
+ * @age:  the age of the dog
+ * @owner: the owner of the dog
+ *
+ * Return: NULL
+ */
+struct dog {
+	char *name;
+	float age;
+	char *owner;
+};
+
+int main(void)
+{
+	struct dog user;
+
+	return (0);
+}
