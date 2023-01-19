18bc06ea8c67e593fac00ac3e99a05dc138d77f9
solamsalu <solamsalu@gmail.com>
2023-01-19 08:48:19 -0800

    Task 1

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_numbers - Entry Point
+ * @separator: comma space
+ * @n: elements to be printed
+ * Return: void
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list slist;
+
+	va_start(slist, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(slist, int));
+		if (i != n - 1 && separator != NULL)
+		{
+			printf("%s", separator);
+		}
+		else
+		{
+			printf("\n");
+		}
+	}
+	va_end(slist);
+}
