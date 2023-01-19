4af0dfa92a6fa6c24686eb9ff799cfc3ff4d6e79
solamsalu <solamsalu@gmail.com>
2023-01-19 09:09:30 -0800

    Task 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,7 +12,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
 	va_list slist;
-	
+
 	if (n > 0)
 	{
 		va_start(slist, n);
@@ -20,7 +20,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		for (i = 0; i < n; i++)
 		{
 			printf("%d", va_arg(slist, int));
-			
+
 			if (i != n - 1 && separator != NULL)
 				printf("%s", separator);
 		}
