bbb32582abf0193c955da8d18a4c22e521a18221
solamsalu <solamsalu@gmail.com>
2023-02-06 07:00:37 -0800

    Task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,37 +1,38 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include "main.h"
-
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 /**
- * create_file - function that creates a file.
- * @filename: variable pointer
- * @text_content: content file
- * Description: Create a function that creates a file.
+ * create_file - creates a file and puts text in it
+ * with 600 perms (do not change if it exists)
+ *
+ * @filename: name for file
+ * @text_content: text to put into file
+ *
  * Return: 1 on success, -1 on failure
  */
-
 int create_file(const char *filename, char *text_content)
 {
-	int i = 0, file;
+	int file;
+	int length = 0, inlen = 0;
+	char *ptr;
 
 	if (filename == NULL)
 		return (-1);
 
-	if (text_content == NULL)
-		text_content = "";
-
-
-	while (text_content[i] != '\0')
-	{
-		i++;
-	}
-
-	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-
+	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (file == -1)
 		return (-1);
 
-	write(file, text_content, i);
+	if (text_content != NULL)
+	{
+		for (inlen = 0, ptr = text_content; *ptr; ptr++)
+			inlen++;
+		length = write(file, text_content, inlen);
+	}
 
+	if (close(file) == -1 || inlen != length)
+		return (-1);
 	return (1);
 }
