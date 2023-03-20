2d10e12af18ed5e2e236f26571b4e36b816e55ba
solamsalu <solamsalu@gmail.com>
2023-03-20 00:23:21 -0700

    Task 0

---
+++ 0x18-dynamic_libraries/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *main - prints _putchar, followed by a new lines
+ *Return: always 0 (Success)
+ */
+int main(void)
+{
+	char sol[] = "_putchar";
+
+	int c;
+
+	for (c = 0; c < 8; c++)
+	{
+		_putchar(sol[c]);
+
+	}
+	_putchar ('\n');
+	return (0);
+
+}
