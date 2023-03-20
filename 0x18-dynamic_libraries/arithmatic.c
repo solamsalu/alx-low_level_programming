7bad237e2b01e56a6c00a5851e862594445e4478
solamsalu <solamsalu@gmail.com>
2023-03-20 11:04:45 -0700

    Task 2

---
+++ 0x18-dynamic_libraries/arithmatic.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "arithmatic.h"
+
+int add(int a, int b)
+{
+    int nAdd = a + b;
+    return nAdd;
+}
+
+int sub(int a, int b)
+{
+    int nSub = a - b;
+    return nSub;
+}
+
+int mul(int a, int b)
+{
+    int nMul = a * b;
+    return nMul;
+}
+
+int mod(int a, int b)
+{
+    int nMod = a % b;
+    return nMod;
+}
---
+++ 0x18-dynamic_libraries/arithmatic.h
@@ -0,0 +1,9 @@
+#ifndef _ARITHMATIC_
+#define _ARITHMATIC_
+
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int mod(int a, int b);
+
+#endif
